e81dce7bf577c0747758f38916bdaa3596b1050e
José Sousa <zpedro_s@hotmail.com>
2023-08-27 00:35:17 +0100

    2022 day 4 part 1

---
+++ 2022/day4/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	p1, err := part1()
+	if err != nil {
+		fmt.Println("error executing part 1")
+	}
+
+	fmt.Printf("Part 1 anwser is: %v\n", p1)
+
+	// p2, err := part2()
+	// if err != nil {
+	// 	fmt.Println("error executing part 2")
+	// }
+
+	// fmt.Printf("Part 2 anwser is: %v\n", p2)
+}
