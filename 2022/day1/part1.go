48b6aa8c7699c3732947c96b89e534d50de426cd
José Sousa <zpedro_s@hotmail.com>
2023-09-03 19:02:18 +0100

    oopsies

--- 2022/day1/part1.go
+++ 2022/day1/part1.go
@@ -1,31 +1,39 @@
 package main
 
-// func part1() (int, error) {
-// 	file, err := os.Open("input")
-// 	if err != nil {
-// 		fmt.Printf("Error reading file %v", file.Name())
-// 	}
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-// 	defer file.Close()
+func part1() (int, error) {
+	file, err := os.Open("input")
+	if err != nil {
+		fmt.Printf("Error reading file %v", file.Name())
+	}
 
-// 	sc := bufio.NewScanner(file)
-// 	currentCal := 0
-// 	mostCaloriesElf := 0
-// 	for sc.Scan() {
-// 		line := sc.Text()
-// 		if line == "" {
-// 			if currentCal > mostCaloriesElf {
-// 				mostCaloriesElf = currentCal
-// 			}
-// 			currentCal = 0
-// 			continue
-// 		}
-// 		cal, err := strconv.Atoi(line)
-// 		if err != nil {
-// 			return 0, errors.New("invalid calorie value")
-// 		}
-// 		currentCal += cal
-// 	}
+	defer file.Close()
 
-// 	return currentCal, nil
-// }
+	sc := bufio.NewScanner(file)
+	currentCal := 0
+	mostCaloriesElf := 0
+	for sc.Scan() {
+		line := sc.Text()
+		if line == "" {
+			if currentCal > mostCaloriesElf {
+				mostCaloriesElf = currentCal
+			}
+			currentCal = 0
+			continue
+		}
+		cal, err := strconv.Atoi(line)
+		if err != nil {
+			return 0, errors.New("invalid calorie value")
+		}
+		currentCal += cal
+	}
+
+	return currentCal, nil
+}
