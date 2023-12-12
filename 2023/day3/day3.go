b8a6d82f1cdf00ec303ad3dc7fdfb400608b5112
José Sousa <jose.sousa@farfetch.com>
2023-12-12 14:44:51 +0000

    2023 day 3 part 2

--- 2023/day3/day3.go
+++ 2023/day3/day3.go
@@ -10,6 +10,7 @@ import (
 
 const symbol = -1
 const dot = -2
+const gear = -3
 
 type point struct {
 	x int
@@ -17,19 +18,19 @@ type point struct {
 }
 
 type rowNumber struct {
-	value   int
-	indices []point
+	value  int
+	points []point
 }
 
 func part1() (int, error) {
 
 	sum := 0
-	matrix := createMatrix()
+	matrix := createMatrix1()
 
 	for x := 0; x < len(matrix[0]); x++ {
 		rowNumbers := readRowNumbers(matrix, x)
 		for _, rowNumber := range rowNumbers {
-			for _, indice := range rowNumber.indices {
+			for _, indice := range rowNumber.points {
 				adjacents := getAdjacents(matrix, indice.x, indice.y)
 				if hasSymbol(adjacents) {
 					sum += rowNumber.value
@@ -40,6 +41,100 @@ func part1() (int, error) {
 	}
 
 	return sum, nil
+}
+
+func part2() (int, error) {
+
+	sum := 0
+	matrix := createMatrix2()
+
+	for x := 0; x < len(matrix[0]); x++ {
+		gears := readGears(matrix, x)
+		fmt.Println(gears)
+		for _, gear := range gears {
+			adjancentParts := readAdjacentNumbers(matrix, gear)
+			if len(adjancentParts) == 2 {
+				sum += adjancentParts[0].value * adjancentParts[1].value
+			}
+		}
+	}
+
+	return sum, nil
+}
+
+func readAdjacentNumbers(matrix [][]int, gear point) []rowNumber {
+	var adjacentNumbers []rowNumber
+	var numbers []rowNumber
+
+	numbers = readRowNumbers(matrix, gear.x)
+
+	// read next line numbers
+	if gear.x < len(matrix)-1 {
+		numbers = append(numbers, readRowNumbers(matrix, gear.x+1)...)
+	}
+
+	// read previous next line numbers
+	if gear.x > 0 {
+		numbers = append(numbers, readRowNumbers(matrix, gear.x-1)...)
+	}
+
+	for _, number := range numbers {
+		if numberIsAdjacentToGear(gear, number) {
+			adjacentNumbers = append(adjacentNumbers, number)
+		}
+	}
+
+	return adjacentNumbers
+}
+
+func numberIsAdjacentToGear(gear point, number rowNumber) bool {
+	for _, point := range number.points {
+		if arePointsAdjacent(gear, point) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// we can assume these points are on the same row or the row above or bellow
+func arePointsAdjacent(p1 point, p2 point) bool {
+	if p1.y == p2.y ||
+		p1.y == p2.y-1 ||
+		p1.y == p2.y+1 {
+		return true
+	}
+
+	return false
+}
+
+func readSameLineAdjacentNumbers(matrix [][]int, gear point) []rowNumber {
+	var rowNumbers []rowNumber
+
+	return rowNumbers
+}
+
+func readUpperLineAdjacentNumbers(matrix [][]int, gear point) []rowNumber {
+	var rowNumbers []rowNumber
+
+	return rowNumbers
+}
+
+func readLowerLineAdjacentNumbers(matrix [][]int, gear point) []rowNumber {
+	var rowNumbers []rowNumber
+
+	return rowNumbers
+}
+
+func readGears(matrix [][]int, row int) []point {
+	var gears []point
+	for i := 0; i < len(matrix); i++ {
+		if matrix[row][i] == gear {
+			gears = append(gears, point{x: row, y: i})
+		}
+	}
+
+	return gears
 }
 
 func hasSymbol(s []int) bool {
@@ -61,7 +156,7 @@ func readRowNumbers(matrix [][]int, row int) []rowNumber {
 	for i := 0; i < len(rowSlice); i++ {
 		if rowSlice[i] >= 0 {
 			previous = append(previous, rowSlice[i])
-			number.indices = append(number.indices, point{x: row, y: i})
+			number.points = append(number.points, point{x: row, y: i})
 			if i == len(rowSlice)-1 {
 				val := previous[0]
 				for i := 1; i < len(previous); i++ {
@@ -89,9 +184,9 @@ func readRowNumbers(matrix [][]int, row int) []rowNumber {
 	return rowNumbers
 }
 
-func createMatrix() [][]int {
+func createMatrix1() [][]int {
 
-	file, err := os.Open("input")
+	file, err := os.Open("input1")
 	if err != nil {
 		fmt.Printf("Error reading file %v", file.Name())
 	}
@@ -121,7 +216,44 @@ func createMatrix() [][]int {
 		matrix = append(matrix, slice)
 	}
 
-	fmt.Println(matrix)
+	return matrix
+}
+
+func createMatrix2() [][]int {
+
+	file, err := os.Open("input2")
+	if err != nil {
+		fmt.Printf("Error reading file %v", file.Name())
+	}
+
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	matrix := [][]int{}
+	for sc.Scan() {
+		line := sc.Text()
+		slice := []int{}
+		for _, char := range line {
+			if unicode.IsDigit(char) {
+				num, _ := strconv.Atoi(string(char))
+				slice = append(slice, num)
+				continue
+			}
+			if char == '.' {
+				slice = append(slice, dot)
+				continue
+			}
+			if char == '*' {
+				slice = append(slice, gear)
+				continue
+			}
+
+			slice = append(slice, symbol)
+		}
+
+		matrix = append(matrix, slice)
+	}
 
 	return matrix
 }
@@ -206,21 +338,3 @@ func getAdjacents(matrix [][]int, x int, y int) []int {
 
 	return result
 }
-
-// func part2() (int, error) {
-// 	file, err := os.Open("input")
-// 	if err != nil {
-// 		fmt.Printf("Error reading file %v", file.Name())
-// 	}
-
-// 	defer file.Close()
-
-// 	sc := bufio.NewScanner(file)
-// 	result := 0
-
-// 	for sc.Scan() {
-// 		line := sc.Text()
-// 	}
-
-// 	return result, nil
-// }
