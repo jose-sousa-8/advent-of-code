4debcabda47498b43ff82a3756c32dd4a678401d
José Sousa <zpedro_s@hotmail.com>
2024-12-04 10:35:46 +0000

    advent of code day 3 part 2

--- 2024/day_three/main.go
+++ 2024/day_three/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 //go:embed input
@@ -20,4 +21,29 @@ func main() {
 		result += x * y
 	}
 	fmt.Println(result)
+
+	fmt.Println(part2())
+}
+
+func part2() int {
+	pattern := `(do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\))`
+	re := regexp.MustCompile(pattern)
+
+	matches := re.FindAllString(text, -1)
+
+	mulEnabled := true
+	result := 0
+	for _, match := range matches {
+		if match == "do()" {
+			mulEnabled = true
+		} else if match == "don't()" {
+			mulEnabled = false
+		} else if strings.HasPrefix(match, "mul(") && mulEnabled {
+			var x, y int
+			fmt.Sscanf(match, "mul(%d,%d)", &x, &y)
+			result += x * y
+		}
+	}
+
+	return result
 }
